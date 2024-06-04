59f5906b7ece40081a65425ce7140dee325b8bd6
Gareth Dean <gazdean@hotmail.com>
2024-06-04 13:27:45 +0100

    function to create event split for seperate api calls

--- src/utils.js
+++ src/utils.js
@@ -2,8 +2,25 @@ export function formatCreateEventData(data) {
     const body = {
         event :{
             listed:false,
-            currency: 'GBP',
-            // ticket_classes: [{
+            currency: 'GBP',          
+        }
+    }
+
+    if (!Object.keys(data).length) return {}
+    else {
+        body.event.name = {html:`<p>${data.name}</p>`}
+        body.event.description = {html:`<p>${data.description}</p>`}
+        body.event.start = {timezone: "Europe/London", utc: `${data.start}:00Z`}
+        body.event.end = {timezone: "Europe/London", utc: `${data.end}:00Z`}
+        body.event.capacity = Number(data.capacity)
+        body.event.category_id = data.category_id      
+    }
+  return body
+  
+}
+
+
+ // ticket_classes: [{
             //     name: "General Admission",
             //     quantity_total: 0,
             //     cost: {
@@ -17,18 +34,9 @@ export function formatCreateEventData(data) {
             //         "country": "UK"
             //     }
             // }
-        }
-    }
 
-    if (!Object.keys(data).length) return {}
-    else {
-        body.event.name = {html:`<p>${data.name}</p>`}
-        body.event.description = {html:`<p>${data.description}</p>`}
-        body.event.start = {timezone: "Europe/London", utc: `${data.start}:00Z`}
-        body.event.end = {timezone: "Europe/London", utc: `${data.end}:00Z`}
-        body.event.capacity = Number(data.capacity)
-        body.event.category_id = data.category_id
-        // body.event.venue =  {
+
+     // body.event.venue =  {
         //                         name: data.venueNameInput,
         //                         address: {...body.event.venue.address,
         //                             address_1: data.address_1,
@@ -44,7 +52,3 @@ export function formatCreateEventData(data) {
         //         body.event.ticket_classes[0].cost.value = data.cost
         //         body.event.ticket_classes[0].free = false
         // }
-    }
-  return body
-  
-}
