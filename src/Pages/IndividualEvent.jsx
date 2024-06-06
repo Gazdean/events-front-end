c08ff1143656f38f0101ff506e03a75cbc6bba07
Gareth Dean <gazdean@hotmail.com>
2024-06-06 20:51:12 +0100

    button added

--- src/Pages/IndividualEvent.jsx
+++ src/Pages/IndividualEvent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container, Image, Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 import { useAuth } from "../Contexts/AuthContext";
 import { fetchEventTicketClasses, fetchIndividualEvent } from "../apiEventBriteCalls";
@@ -58,7 +58,8 @@ export default function IndividualEvent({ organizationId, images }) {
                         <p>{event.summary}</p>
                         {loading? <p>--loading--</p> : eventTickets.free ? <p style={{color:"green"}}>free event</p> : eventTickets.donation ? <p style={{color:"blue"}}>donation</p> : <p style={{color:"red"}}>Price: {eventTickets.cost.display}</p>}
                         {loading? <p>--loading--</p> : <p style={{color:"green"}}>Tickets Available: {eventTickets.quantity_total < 5 && eventTickets.quantity_total > 0 ? 'Nearly Sold Out!!' : eventTickets.quantity_total == 0 ? 'Sold Out!!!!!' : eventTickets.quantity_total }</p> }
-                        {currentUser ? <><Button onClick={handleShow}>Sign Up</Button><SignUpModal setShow={setShow} show={show} />   </> : null}
+                        <Link to={`/`}><Button className="me-5">Return to Events</Button></Link>
+                        {currentUser ? <><Button className="ms-5" onClick={handleShow}>Sign Up</Button><SignUpModal setShow={setShow} show={show} />   </> : null}
                     </Col>
                 </Row>
             )
