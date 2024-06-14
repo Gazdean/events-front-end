618165519188367068b8a4845690e0fc809e1c6f
Gareth Dean <gazdean@hotmail.com>
2024-06-14 14:39:16 +0100

    header with collaspable menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,15 @@
-import { Button } from 'react-bootstrap';
 import React, { useEffect, useState } from 'react'
-import { useAuth } from '../Contexts/AuthContext'
-
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useAuth } from '../Contexts/AuthContext'
 import { Link, useLocation } from 'react-router-dom';
 import { querySnapshot } from '../apiFirebaseCalls';
 
-export default function Header() {
-  const [error, setError] = useState("")
+export default function HeaderTest() {
+    const [error, setError] = useState("")
   const location = useLocation();
   const {currentUser, logout} = useAuth()
   const [isStaff, setIsStaff] = useState(false)
@@ -18,6 +19,7 @@ export default function Header() {
     setError("")
     try {
         await logout()
+        setIsStaff(false)
     } catch {
         setError('Failed to log out')
     }
@@ -35,39 +37,54 @@ export default function Header() {
       const querySnapShot = await querySnapshot("staff", email)
       if (querySnapShot.exists()) {
         setIsStaff(true)
-        // console.log("Staff member exists");
-
+        console.log("Staff member exists");
       } else {
-        // console.log("Staff member does not exist");
+        setIsStaff(false)
+        console.log("Staff member does not exist");
       }
     } catch (error) {
       console.error("Error checking if staff exists: ", error);
     }
   };
  
-    
   return (
-    <>
-    <Navbar className="m-3 border-bottom ">
-        <Navbar.Brand style={{ color: "#429DD0" }} as={Link} to={"/"} ><strong>Gather</strong></Navbar.Brand>    
-        <Nav className="ms-auto d-flex flex-row justify-content-center align-contents-center">
-            {isStaff ?<Nav.Link as={Link} to={"/create-event"}>
-                <Button variant="primary" size="sm">Create Event</Button>
-              </Nav.Link > : null
-            }
-            {currentUser ? <>
-              {error ? <p>{error}</p> : null}
-              <Nav.Link as={Link} to={"/profile"}>
-                <Nav.Item className='pt-2 me-2' style={{fontSize:"0.9rem"}}>{currentUser.email}</Nav.Item>
-              </Nav.Link >
-              <Button variant="primary" size="sm" onClick={handleLogout}>logout</Button>
-            </> : <> 
-              {location.pathname !== "/sign-in" ? <Nav.Link as={Link} to={"/sign-in"}><Button variant="primary" size="sm">Sign in</Button></Nav.Link> : null }
-              {location.pathname !== "/join" ? <Nav.Link as={Link} to={"/join"}><Button variant="primary" size="sm" style={{width:"3.9rem"}}>Join</Button></Nav.Link> : null }
-            </> }
-        </Nav>
-        
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary" fixed="top" >
+      <Container>
+        <Navbar.Brand style={{ color: "#429DD0" }} as={Link} to={"/"}  ><strong>Gather</strong></Navbar.Brand>
+         {error ? <p>{error}</p> : null}
+         {!currentUser ?
+            <>  <Nav>
+                {location.pathname !== "/sign-in" ? 
+                    (<Nav.Link as={Link} to={"/sign-in"}>
+                        <Button variant="primary" size="sm">Sign in</Button>
+                    </Nav.Link> ): 
+                    null 
+                }
+                {location.pathname !== "/join" && <Nav.Link as={Link} to={"/join"}><Button variant="primary" size="sm" style={{width:"3.9rem"}}>Join</Button></Nav.Link>}
+                </Nav>
+            </> : 
+            <>
+                <Nav.Link as={Link} to={"/profile"}>
+                    <Nav.Item className='pt-2 me-2 ' style={{fontSize:"0.9rem"}}>{currentUser.email}</Nav.Item>
+                </Nav.Link >
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav className="ms-auto">  
+                        {isStaff && 
+                            <Nav.Link as={Link} to={"/create-event"}>
+                                <Button variant="primary" size="sm">Create Event</Button>
+                            </Nav.Link >
+                        }
+                        {currentUser && 
+                            <Nav.Link>
+                                <Button variant="primary" size="sm" onClick={handleLogout}>Logout</Button>
+                            </Nav.Link>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+                </>
+        }
+      </Container>
     </Navbar>
-    </>
-  )
+  );
 }
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -6,10 +6,10 @@ import { Col, Container, Row, Image } from "react-bootstrap";
 export default function Landing({ images, categories, events, eventsTickets, setEventsTickets, imagesLoading }) {
  
   return (
-    <Container>
+    <Container className="mt-3">
       <Row>
         
-        <Col sm={6}><Image width='100%' height='auto' src={images?.landing?.full} alt="cafe picture"/> {imagesLoading  && <p>-- Image Loading --</p>}  </Col>
+        <Col sm={6}><Image className="mt-4"width='100%' height='auto' src={images?.landing?.full} alt="cafe picture"/> {imagesLoading  && <p>-- Image Loading --</p>}  </Col>
         <Col sm={6}><CallToAction /></Col>
       </Row>
       <Row>
